96672fbb724f6b0b28fd4bfb9afa0366f097499d
Stanis1av <krasavin.stanislaus@gmail.com>
2021-11-10 23:30:24 +0300

    Creating a language switcher Part 1. Creating embedded_svg helper to use svg icons with options.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def embedded_svg(icon_name, options={})
+    file = File.read(Rails.root.join('app', 'assets', 'images', 'flags', "#{icon_name}.svg"))
+    doc = Nokogiri::HTML::DocumentFragment.parse file
+    svg = doc.at_css 'svg'
+
+    options.each {|attr, value| svg[attr.to_s] = value}
+
+    doc.to_html.html_safe
+  end
 end
